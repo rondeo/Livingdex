034c37df51ecb9d13d2b2ad1b681cfbc72c2599f
Nicholas Rahbany <nrahbany22@yahoo.com>
2019-03-23 21:44:21 -0400

    Changed background colors per each generation

--- src/app/public/home-page/home-page.component.ts
+++ src/app/public/home-page/home-page.component.ts
@@ -10,14 +10,33 @@ export class HomePageComponent implements OnInit {
   constructor() { }
 
   generations: any = [
-    { name: "Generation 1" },
-    { name: "Generation 2" },
-    { name: "Generation 3" },
-    { name: "Generation 4" },
-    { name: "Generation 5" },
-    { name: "Generation 6" },
-    { name: "Generation 7" }
+    { name: "Generation 1", number: 1 },
+    { name: "Generation 2", number: 2 },
+    { name: "Generation 3", number: 3 },
+    { name: "Generation 4", number: 4 },
+    { name: "Generation 5", number: 5 },
+    { name: "Generation 6", number: 6 },
+    { name: "Generation 7", number: 7 }
   ]
+
+  getColor(genNumber: number) {
+    switch (genNumber) {
+      case 1:
+        return 'red';
+      case 2:
+        return 'gold';
+      case 3:
+        return 'blue';
+      case 4:
+        return 'pink';
+      case 5:
+        return 'white';
+      case 6:
+        return 'blue';
+      case 7:
+        return 'yellow';
+    }
+  }
 
   ngOnInit() {
   }
