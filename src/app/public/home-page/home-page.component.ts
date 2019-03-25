96fd26aa334f2ffb9d07a55a85015aee61c0ed5c
Nicholas Rahbany <nrahbany22@yahoo.com>
2019-03-24 20:56:34 -0400

    Commented out images, will update design later

--- src/app/public/home-page/home-page.component.ts
+++ src/app/public/home-page/home-page.component.ts
@@ -10,33 +10,26 @@ export class HomePageComponent implements OnInit {
   constructor() { }
 
   generations: any = [
-    { name: "Generation 1", number: 1 },
-    { name: "Generation 2", number: 2 },
-    { name: "Generation 3", number: 3 },
-    { name: "Generation 4", number: 4 },
-    { name: "Generation 5", number: 5 },
-    { name: "Generation 6", number: 6 },
-    { name: "Generation 7", number: 7 }
+    { number: 1 },
+    { number: 2 },
+    { number: 3 },
+    { number: 4 },
+    { number: 5 },
+    { number: 6 },
+    { number: 7 },
+    { number: 8 }
   ]
 
-  getColor(genNumber: number) {
-    switch (genNumber) {
-      case 1:
-        return 'red';
-      case 2:
-        return 'gold';
-      case 3:
-        return 'blue';
-      case 4:
-        return 'pink';
-      case 5:
-        return 'white';
-      case 6:
-        return 'blue';
-      case 7:
-        return 'yellow';
-    }
-  }
+  // generations: any = [
+  //   { number: 1, image_url: 'https://cdn.bulbagarden.net/upload/8/86/Kanto_Town_Map_RBY.png' },
+  //   { number: 2, image_url: 'http://i.imgur.com/qWGFhK9.png' },
+  //   { number: 3, image_url: 'https://cdn.bulbagarden.net/upload/thumb/1/1f/Hoenn_RSE_Map.png/250px-Hoenn_RSE_Map.png' },
+  //   { number: 4, image_url: 'https://cdn.bulbagarden.net/upload/thumb/6/6a/Sinnoh_Region.png/300px-Sinnoh_Region.png' },
+  //   { number: 5, image_url: 'https://cdn.bulbagarden.net/upload/thumb/b/b4/Unova.png/250px-Unova.png' },
+  //   { number: 6, image_url: 'https://www.serebii.net/pokearth/kalos.png' },
+  //   { number: 7, image_url: 'https://www.serebii.net/pokearth/alola.png' },
+  //   { number: 8, image_url: 'https://swordshield.pokemon.com/assets/img/common/map.jpg' }
+  // ]
 
   ngOnInit() {
   }
